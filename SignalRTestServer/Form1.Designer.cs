9565561cd12be93ca805b2b5510ab0a2e25380d8
Johan Lindqvist <Johan.Lindqvist@infracontrol.com>
2016-02-02 11:32:33 +0100

    Add project

---
+++ SignalRTestServer/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SignalRTestServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 38);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(131, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(150, 38);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(131, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(12, 12);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(269, 20);
+            this.addressTextBox.TabIndex = 2;
+            this.addressTextBox.Text = "http://*:8500/signalr";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 71);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SignalR Test Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox addressTextBox;
+    }
+}
+
---
+++ SignalRTestServer/Form1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalRTestServer
+{
+    public partial class Form1 : Form
+    {
+        IDisposable server;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (server == null)
+            {
+                server = WebApp.Start(addressTextBox.Text, app =>
+                {
+                    app.UseCors(CorsOptions.AllowAll);
+                    app.MapSignalR("", new HubConfiguration
+                    {
+                        // You can enable JSONP by uncommenting line below.
+                        // JSONP requests are insecure but some older browsers (and some
+                        // versions of IE) require JSONP to work cross domain
+                        EnableJSONP = true
+                    });
+                });
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (server != null)
+            {
+                server.Dispose();
+                server = null;
+            }
+        }
+    }
+}
